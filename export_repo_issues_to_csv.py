29cabebc718f3ea1cf66f6094d516bb598e87473
Kyle Tilman <kyletilman@gmail.com>
2013-10-14 10:31:10 -0400

    Modify the output to include additional fields

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -19,14 +19,16 @@ def write_issues(response):
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        print issue
-        csvout.writerow([issue['number'], issue['title'].encode('utf-8'), issue['html_url']])
+        labels = issue['labels']
+        for label in labels:
+            if label['name'] == "Client Requested":
+                csvout.writerow([issue['number'], issue['title'].encode('utf-8'), issue['body'].encode('utf-8'), issue['created_at'], issue['updated_at']])
 
 
 r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
 csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
 csvout = csv.writer(open(csvfile, 'wb'))
-csvout.writerow(('id', 'Title', 'URL'))
+csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
 write_issues(r)
 
 #more pages? examine the 'link' header returned
