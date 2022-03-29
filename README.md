5b7538ef6c91805b3de0922382604befc146e8cd
Stiff Neck Jim <22213990+stiffneckjim@users.noreply.github.com>
2022-03-29 21:08:00 +0100

    Switched from `ini` to `json` for storing the configuration.

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # export_zenhub
+
 Exports Issues from a list of repositories to individual CSV files
 Uses basic authentication (Github API Token and Zenhub API Token)
 to retrieve Issues from a repository that token has access to.
 
 Supports Github API v3 and ZenHub's current working API.
 
-Forked from https://gist.github.com/Jammizzle/ad2a94008b56a6f9d17cfdddf5a6dd4d
+Forked from [ceresfairfood/export_zenhub](https://github.com/ceresfairfood/export_zenhub) which was forked from [a gist by Jammizzle](https://gist.github.com/Jammizzle/ad2a94008b56a6f9d17cfdddf5a6dd4d)
 
 ## Config
-Add the following to config.ini with appropriate values:
-```
-[DEFAULT]
-AUTH_TOKEN = # your personal GitHub token
-ZEN_ACCESS = # the personal token provided by Zenhub
 
-QUERY = # See https://developer.github.com/v3/issues/#list-repository-issues
-FILENAME = /Users/$USER/Downloads/export_zenhub.csv
+Add the following to config.json with appropriate values:
+
+```json
+{
+    "GH_ACCESS": "your personal GitHub API token",
+    "ZEN_ACCESS": "the personal token provided by Zenhub",
+    "QUERY": "See https://developer.github.com/v3/issues/#list-repository-issues"
+    "FILENAME": "/Users/$USER/Downloads/export_zenhub.csv"
+    "GH_REPO": "your GitHub repo",
+    "ZEN_BOARD": "the corresponding ZenHub board"
+}
 ```
 
 To build the query, you can use the web interface, but you need to translate some of the options). For example:
@@ -29,13 +34,16 @@ Is equivalent to
 ## Usage
 
 Just execute the Python script:
-```
+
+```Shell
 python export_multi_repo_issues_to_csv.py
 ```
 
-## todo
-1. Add a command-line parameter for filtering by date
-2. add a help message for when I forget how to use above parameter ;)
-4. why not output to sdtout?
-5. Move repo list to config (REPO_LIST)
-6. Optional config to whitelist included fields (eg: `FIELDS=number,title,assignees,description` )
+## TODO
+
+[ ] Add back handling multiple GitHub repos and ZenHub boards
+[ ] Add command-line parameters, including for filtering by date
+[ ] Add a help message for those parameters
+[ ] Flag to output to STDOUT
+[x] Move repo list to config (REPO_LIST)
+[ ] Optional config to whitelist included fields (eg: "FIELDS": "number,title,assignees,description")
--- export_multi_repo_issues_to_csv.py
+++ export_multi_repo_issues_to_csv.py
@@ -2,7 +2,6 @@
 import csv
 import json
 import requests
-import configparser
 
 """
 Usage:
@@ -23,110 +22,153 @@ Derived from https://gist.github.com/Kebiled/7b035d7518fdfd50d07e2a285aff3977
 @PinnaclePSM Author Jamie Belcher
 """
 
-def write_issues(r, csvout, repo_name, repo_ID):
+
+def write_issues(r, csvout, gh_repo, zen_board, zen_access):
     if not r.status_code == 200:
-        raise Exception("Request returned status of:"+str(r.status_code))
+        raise Exception("Request returned status of:" + str(r.status_code))
 
     r_json = r.json()
-    print(str(len(r_json)) + ' issues loaded. Loading ZenHub data...')
+    print(str(len(r_json)) + " issues loaded. Loading ZenHub data...")
     for issue in r_json:
-        print(repo_name + ' issue: ' + str(issue['number']))
+        print(gh_repo + " issue: " + str(issue["number"]))
 
-        zenhub_issue_url = 'https://api.zenhub.io/p1/repositories/' + str(repo_ID) + '/issues/' + str(issue['number']) + '?access_token=' + ACCESS_TOKEN
+        zenhub_issue_url = (
+            "https://api.zenhub.io/p1/repositories/"
+            + zen_board
+            + "/issues/"
+            + str(issue["number"])
+            + "?access_token="
+            + zen_access
+        )
         zen_r = requests.get(zenhub_issue_url).json()
-        DateCreated = issue['created_at'][:-10]
-        DateUpdated = issue['updated_at'][:-10]
-        if 'pull_request' not in issue:
+        DateCreated = issue["created_at"][:-10]
+        DateUpdated = issue["updated_at"][:-10]
+        if "pull_request" not in issue:
             global ISSUES
             ISSUES += 1
-            assignees, tag, category, priority, labels = '', '', '', '', ''
-            for i in issue['assignees'] if issue['assignees'] else []:
-                assignees += i['login'] + ','
+            assignees, tag, category, priority, labels = "", "", "", "", ""
+            for i in issue["assignees"] if issue["assignees"] else []:
+                assignees += i["login"] + ","
 
-            for x in issue['labels'] if issue['labels'] else []:
-                if "Category" in x['name']:
-                    category = x['name'][11:11 + len(x['name'])]
-                elif "Tag" in x['name']:
-                    tag = x['name'][6:6 + len(x['name'])]
-                elif "Priority" in x['name']:
-                    priority = x['name'][11:11 + len(x['name'])]
+            for x in issue["labels"] if issue["labels"] else []:
+                if "Category" in x["name"]:
+                    category = x["name"][11 : 11 + len(x["name"])]
+                elif "Tag" in x["name"]:
+                    tag = x["name"][6 : 6 + len(x["name"])]
+                elif "Priority" in x["name"]:
+                    priority = x["name"][11 : 11 + len(x["name"])]
                 else:
-                    labels += x['name'] + ','
+                    labels += x["name"] + ","
 
-            estimate = zen_r.get('estimate', dict()).get('value', "")
+            estimate = zen_r.get("estimate", dict()).get("value", "")
 
-            if issue['state'] == 'closed':
-                Pipeline = 'Closed'
+            if issue["state"] == "closed":
+                Pipeline = "Closed"
             else:
-                Pipeline = zen_r.get('pipeline', dict()).get('name', "")
+                Pipeline = zen_r.get("pipeline", dict()).get("name", "")
 
-            if not issue.get('body'):
-                issue['body'] = ''
+            if not issue.get("body"):
+                issue["body"] = ""
 
-            csvout.writerow([repo_name, 
-                            issue['number'], issue['title'].encode('utf-8'),
-                            issue['body'].encode('utf-8'), 
-                            assignees[:-1],
-                            issue['state'],
-                            Pipeline, DateCreated, DateUpdated,
-                            labels[:-1],
-                            category, tag, priority,
-                            estimate])
+            csvout.writerow(
+                [
+                    gh_repo,
+                    issue["number"],
+                    issue["title"].encode("utf-8"),
+                    issue["body"].encode("utf-8"),
+                    assignees[:-1],
+                    issue["state"],
+                    Pipeline,
+                    DateCreated,
+                    DateUpdated,
+                    labels[:-1],
+                    category,
+                    tag,
+                    priority,
+                    estimate,
+                ]
+            )
 
-def get_issues(repo_data):
-    repo_name = repo_data[0]
-    repo_ID = repo_data[1]
-    issues_for_repo_url = 'https://api.github.com/repos/%s/issues?%s' % (repo_name, QUERY)
-    print('Retrieving issues... ' + issues_for_repo_url)
-    r = requests.get(issues_for_repo_url, auth=AUTH)
-    write_issues(r, FILEOUTPUT, repo_name, repo_ID)
+
+def get_issues(gh_repo, query, gh_access, zen_board, fileoutput, zen_access):
+    issues_for_repo_url = "https://api.github.com/repos/%s/issues?%s" % (
+        gh_repo,
+        query,
+    )
+    print("Retrieving issues... " + issues_for_repo_url)
+    r = requests.get(issues_for_repo_url, auth=("token", gh_access))
+    write_issues(
+        r=r,
+        csvout=fileoutput,
+        gh_repo=gh_repo,
+        zen_board=zen_board,
+        zen_access=zen_access,
+    )
     # more pages? examine the 'link' header returned
-    if 'link' in r.headers:
+    if "link" in r.headers:
         pages = dict(
-            [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
-             [link.split(';') for link in
-              r.headers['link'].split(',')]])
-        while 'last' in pages and 'next' in pages:
+            [
+                (rel[6:-1], url[url.index("<") + 1 : -1])
+                for url, rel in [
+                    link.split(";") for link in r.headers["link"].split(",")
+                ]
+            ]
+        )
+        while "last" in pages and "next" in pages:
             pages = dict(
-                [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
-                 [link.split(';') for link in
-                  r.headers['link'].split(',')]])
-            r = requests.get(pages['next'], auth=AUTH)
-            write_issues(r, FILEOUTPUT, repo_name, repo_ID)
-            if pages['next'] == pages['last']:
+                [
+                    (rel[6:-1], url[url.index("<") + 1 : -1])
+                    for url, rel in [
+                        link.split(";") for link in r.headers["link"].split(",")
+                    ]
+                ]
+            )
+            r = requests.get(pages["next"], auth=gh_access)
+            write_issues(r, FILEOUTPUT, gh_repo, zen_board)
+            if pages["next"] == pages["last"]:
                 break
 
 
-REPO_LIST = [("ceresfairfood/fairfood-issues", "70860693")]
+with open("config.json", "r") as config_file:
+    CONFIG = json.load(config_file)
+    print("Read config file successfully")
+    print(CONFIG)
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-AUTH = ('token', config['DEFAULT']['AUTH_TOKEN'])
-ACCESS_TOKEN = config['DEFAULT']['ZEN_ACCESS']
 
 # See https://developer.github.com/v3/issues/#list-repository-issues
-QUERY = config['DEFAULT']['QUERY']
+
 
 ISSUES = 0
-FILENAME = config['DEFAULT']['FILENAME']
-OPENFILE = open(FILENAME, 'w')
+OPENFILE = open(CONFIG["FILENAME"], "w")
 FILEOUTPUT = csv.writer(OPENFILE)
 
-FILEOUTPUT.writerow(('Repository', 
-                     'Issue Number', 'Issue Title',
-                     'Description', 
-                     'Category', 'Tag', 
-                     'Assigned To',
-                     'Priority', 
-                     'State',
-                     'Pipeline',
-                     'Issue Author',
-                     'Created At', 'Updated At', 
-                     'Labels', 'Estimate'
-                     ))
+FILEOUTPUT.writerow(
+    (
+        "Repository",
+        "Issue Number",
+        "Issue Title",
+        "Description",
+        "Category",
+        "Tag",
+        "Assigned To",
+        "Priority",
+        "State",
+        "Pipeline",
+        "Issue Author",
+        "Created At",
+        "Updated At",
+        "Labels",
+        "Estimate",
+    )
+)
 
-for repo_data in REPO_LIST:
-    get_issues(repo_data)
+get_issues(
+    gh_access=CONFIG["GH_ACCESS"],
+    gh_repo=CONFIG["GH_REPO"],
+    zen_board=CONFIG["ZEN_BOARD"],
+    query=CONFIG["QUERY"],
+    zen_access=CONFIG["ZEN_ACCESS"],
+    fileoutput=FILEOUTPUT,
+)
 
 OPENFILE.close()
